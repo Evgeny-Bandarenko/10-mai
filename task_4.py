a10df3887de9b38318496ca48523e8a86483638a
Evgeny <104760685+Evgeny-Bandarenko@users.noreply.github.com>
2022-05-10 23:58:50 +0300

    Add files via upload

---
+++ task_4.py
@@ -0,0 +1,38 @@
+# 4.1
+# nomer = int(input())
+# if nomer <0:
+#     print("Отрицательное")
+# elif nomer >=0:
+#     print("Положительное")
+# 4.2
+# print("Выберете операцию")
+# print("1 - перевод в цельсиях")
+# print("2 - перевод в ференгейты")
+# d = int(input())
+# s = input()
+# if s == "c":
+#     print((d-32)*5/9)
+# if d == 2:
+#     print("Ведите температуру в цельсиях")
+#     b = int(input())
+#     print("температура в фарингейтах")
+#     print((b*9/5)+32)
+# 4.4
+# word_1 =list(input("Введите первое слово:"))
+# word_2 =list(input("Введите втровое слово :"))
+# if (sorted(word_1)==sorted(word_2)):
+#     print("anagram")
+# else:
+#     print("just words")
+# 4.5
+# Ввод:Число и процент и программа должна посчитать процент от числа
+# while True:
+#     try:
+#         number = int(input("Number:"))
+#         procent = int(input("Процент:"))
+#     except ValueError:
+#         print("Введите цифры!")
+#     else:
+#         finish = number / 100 * procent
+#         print("Ваш ответ" , float(finish))
+#         break
---
+++ task_5.py
@@ -0,0 +1,23 @@
+# 5.1
+# mult = 1
+# for i in range(1, 11):
+#     if i % 2 == 1:
+#         mult *=i
+# print(mult)
+
+# mult = 1
+# for i in range(1, 11, 2):
+#         mult *=i
+# print(mult)
+
+# num = int(input())
+# for i in range(1, num +1):
+#     for j in range(1, i+1):
+#         print(j, end=" ")
+#     print()
+
+# nums = [int(i) for i in input().split()]
+# for i in nums:
+#     for j in range(1, i+ 1):
+#         print("*", end="")
+#     print()
